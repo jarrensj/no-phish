518f50148ed231ab0db54131f0170a48c442dd76
Jarren San Jose <sanjose.jarren@gmail.com>
2024-01-05 19:21:29 -0800

    create input box

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">hi</h1>
+      <h1 className="text-4xl font-bold">no-phish</h1>
+      <p className="text-xl mt-2">
+        A simple phishing detection tool. Paste the link below to get started.
+      </p>
+      <div className="flex flex-col items-center mt-4">
+        <input
+          className="w-96 h-12 rounded-md px-4 border-2 border-gray-300"
+          type="text"
+          placeholder="Paste link here"
+        />
+        <button className="mt-4 w-96 h-12 rounded-md bg-blue-500 text-white font-bold">
+          Check
+        </button>
+      </div>
     </main>
   )
 }
